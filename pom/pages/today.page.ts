13efdbfc6321b0368d6b91692ac4692a595f791d
Francisco Mondragón <fjgm11@hotmail.com>
2026-03-11 19:04:06 -0600

    Added test to dismiss AddTask modal#38

--- pom/pages/today.page.ts
+++ pom/pages/today.page.ts
@@ -1,10 +1,12 @@
 import { type Page, type Locator } from "@playwright/test";
 import { BasePage } from "./base.page";
+import { TaskEditor } from "../components/taskEditor.component";
 
 export class TodayPage extends BasePage {
 	readonly titleDate: Locator;
 	readonly taskCounter: Locator;
 	readonly addTaskButton: Locator;
+	readonly taskEditor: TaskEditor;
 	readonly todayRoot: Locator;
 
 	constructor(page: Page) {
@@ -12,12 +14,17 @@ export class TodayPage extends BasePage {
 		this.titleDate = page.locator("span[class=simple_content]");
 		this.taskCounter = page.locator("p[class=board_section__task_count]");
 		this.addTaskButton = page.locator("button[class=plus_add_button]");
+		this.taskEditor = new TaskEditor(page);
 		this.todayRoot = page.getByTestId("today-view");
 	}
 
 	async addTask(title: string, desc: string) {
 		await this.addTaskButton.click();
 		await super.addTask(title, desc);
+	}
+
+	async openAddTaksModal() {
+		await this.addTaskButton.click();
 	}
 
 	async awaitForVisibleRoot() {
--- pom/tests/ui/today.spec.ts
+++ pom/tests/ui/today.spec.ts
@@ -4,6 +4,7 @@ import { USER_CREDENTIALS } from "../../data/Constants";
 import { TodayPage } from "../../pages/today.page";
 import { TASK_DETAILS } from "../../data/Constants";
 import { TaskService } from "../../services/task";
+import { Common } from "../../common/common";
 import { type Task } from "../../data/Interfaces";
 import { TODAY_URL } from "../../data/urls";
 import { DUE_DATE } from "../../data/Constants";
@@ -38,9 +39,7 @@ test.describe("Task Management", async () => {
 
 	test("should be able to create a task correctly", async ({ page }) => {
 		await todayPage.addTask(TASK_DETAILS.TITLE, TASK_DETAILS.DESC);
-
 		taskId = await taskService.getTaskId(TASK_DETAILS.TITLE);
-
 		await expect(page.locator(`[id=task-${taskId}]`)).toBeVisible();
 	});
 
@@ -57,5 +56,32 @@ test.describe("Task Management", async () => {
 		await taskCardComponent.deleteTaskFromMenu();
 		await deleteModalComponent.confirmDelete();
 		await expect(taskCard).not.toBeVisible();
+	});
+});
+
+test.describe("Page Functionality", async () => {
+	let loginPage: LoginPage;
+	let todayPage: TodayPage;
+	let common: Common;
+
+	test.beforeEach(async ({ page }) => {
+		loginPage = new LoginPage(page);
+		todayPage = new TodayPage(page);
+		common = new Common(page);
+
+		await loginPage.login(
+			USER_CREDENTIALS.STANDARD_USER,
+			USER_CREDENTIALS.PASSWORD
+		);
+		await page.waitForURL(TODAY_URL);
+		await todayPage.awaitForVisibleRoot();
+	});
+
+	test("Should dismiss add task modal", async () => {
+		await todayPage.openAddTaksModal();
+		await expect(todayPage.taskEditor.taskEditorForm).toBeVisible();
+		await common.pressESCKey();
+		await todayPage.taskEditor.taskEditorForm.waitFor({ state: "hidden" });
+		await expect(todayPage.taskEditor.taskEditorForm).not.toBeVisible();
 	});
 });
